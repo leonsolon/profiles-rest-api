591dd921d0a207a6fed8d46618f0eb875300b932
Leon <leonsolon@gmail.com>
2020-04-21 21:53:59 -0300

    token authentication

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -10,7 +10,9 @@ router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 """Doesn't need base_name because queryset already create for us based on the model name"""
 router.register('profile', views.UserProfileViewSet)
 
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
